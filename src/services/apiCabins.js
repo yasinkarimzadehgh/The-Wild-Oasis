45a4c9f473f2b92f64968b6c88d7ceb564410e30
Yasin Karimzadeh <yasinkarimzadehgh@gmail.com>
2024-08-04 16:49:39 +0330

    modified

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,7 +4,6 @@ export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
-    console.error(error);
     throw new Error("Cabins could not be loaded");
   }
 
